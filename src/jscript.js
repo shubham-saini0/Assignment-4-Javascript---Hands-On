e0597747d4c2bd9c4d8e1953e714dd36c21b79f7
shubham-saini0 <shubhamsaini@cedcoss.com>
2022-02-05 17:05:48 +0530

    Using Objects for Lookups

--- src/jscript.js
+++ src/jscript.js
@@ -761,4 +761,15 @@ const myDog = {
   };
   delete myDog.tails;
 
+  /* task 90 */
+  function phoneticLookup(val) {
+    let result = "";
+    // Only change code below this line
+   var lookup={"alpha":"Adams","bravo":"Boston","charlie":"Chicago","delta":"Denver","echo":"Easy","foxtrot":"Frank"};
+  result=lookup[val];
+    // Only change code above this line
+    return result;
+  }
+  phoneticLookup("charlie");
+
   

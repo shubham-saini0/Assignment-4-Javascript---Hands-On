1c48258e24686e733a1bdc9f97adee968594c9c5
shubham-saini0 <shubhamsaini@cedcoss.com>
2022-02-05 16:48:11 +0530

    Accessing Object Properties with Variables

--- src/jscript.js
+++ src/jscript.js
@@ -722,4 +722,13 @@ const myDog = {
   // Only change code below this line
   const entreeValue = testObj["an entree"];   // Change this line
   const drinkValue = testObj["the drink"];
-  
+
+  /* task 86 */
+  const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  // Only change code below this line
+  const playerNumber = 16;  // Change this line
+  const player = testObj[playerNumber];

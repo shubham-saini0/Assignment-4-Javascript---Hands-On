b5709ed3aeccb3ace082f92c21c2b33501c925d4
shubham-saini0 <shubhamsaini@cedcoss.com>
2022-02-05 15:11:50 +0530

    Comparisons with the Logical And Operator

--- src/jscript.js
+++ src/jscript.js
@@ -429,4 +429,15 @@ function testStrict(val) {
   }
   testLessOrEqual(10);
 
+ /* task 69 */
+ function testLogicalAnd(val) {
+    // Only change code below this line
+      if (val<=50 && val>=25) {
+        return "Yes";
+      }
+    // Only change code above this line
+    return "No";
+  }
+  testLogicalAnd(10);
+
   

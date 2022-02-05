be440e2c209ecde61311d5f86af758ffbb173eba
shubham-saini0 <shubhamsaini@cedcoss.com>
2022-02-05 15:05:30 +0530

    Comparison with the Less Than Operator

--- src/jscript.js
+++ src/jscript.js
@@ -405,4 +405,16 @@ function testStrict(val) {
   }
   testGreaterOrEqual(10);
 
+ /* task 67 */
+ function testLessThan(val) {
+    if (val<25) {  // Change this line
+      return "Under 25";
+    }
+    if (val<55) {  // Change this line
+      return "Under 55";
+    }
+    return "55 or Over";
+  }
+  testLessThan(10);
+
   

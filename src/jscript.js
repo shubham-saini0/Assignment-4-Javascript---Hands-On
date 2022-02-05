a86d225e89675568afd1f949fb4d281488e1e7c7
shubham-saini0 <shubhamsaini@cedcoss.com>
2022-02-05 16:04:44 +0530

    Adding a Default Option in Switch Statements

--- src/jscript.js
+++ src/jscript.js
@@ -560,3 +560,26 @@ function caseInSwitch(val) {
     return answer;
   }
   caseInSwitch(1);
+
+  /* task 77 */
+  function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+  switch (val) {
+    case "a":
+      answer="apple";
+      break;
+    case "b":
+      answer="bird";
+      break;
+      case "c":
+  answer="cat";
+  break;
+    default:
+      answer="stuff";
+      break;
+  }
+    // Only change code above this line
+    return answer;
+  }
+  switchOfStuff(1);

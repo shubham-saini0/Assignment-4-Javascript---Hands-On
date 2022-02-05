1f6e523bd2ffbd97e2d9f03719b8f79a5b848f5e
shubham-saini0 <shubhamsaini@cedcoss.com>
2022-02-05 15:43:21 +0530

    Quoting Strings with Single Quotes

--- src/jscript.js
+++ src/jscript.js
@@ -501,4 +501,25 @@ function testElseIf(val) {
   orderMyLogic(11);
 
   /* task 74 */
+  function testSize(num) {
+    // Only change code below this line
+  if (num<5){return "Tiny";}
+  else if (num <10){return "Small";}
+  else if (num <15){return "Medium";}
+  else if (num <20){return "Large";}
+  else if (num>=20){return "Huge";}
+    return "Change Me";
+    // Only change code above this line
+  }
+  testSize(0);
+  testSize(4);
+  testSize(5);
+  testSize(8);
+  testSize(10);
+  testSize(14);
+  testSize(15);
+  testSize(17);
+  testSize(20);
+  testSize(25);
+
   

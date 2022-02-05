9a3579e5c37b1d91ff15a5a91788242f8291c5c6
shubham-saini0 <shubhamsaini@cedcoss.com>
2022-02-05 16:27:30 +0530

    Return Early Pattern for Functions

--- src/jscript.js
+++ src/jscript.js
@@ -644,4 +644,13 @@ function caseInSwitch(val) {
     // Only change code above this line
   }
   isLess(10, 15);
-  
+
+  /* task 81 */
+  function abTest(a, b) {
+    // Only change code below this line
+  if(a<0||b<0)
+  return undefined;
+    // Only change code above this line
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  abTest(2,2);

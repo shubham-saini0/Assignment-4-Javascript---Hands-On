57edb8bedf112165df71cfca5ee46a230836b844
shubham-saini0 <shubhamsaini@cedcoss.com>
2022-02-05 15:01:26 +0530

    Comparison with the Greater Than Operator

--- src/jscript.js
+++ src/jscript.js
@@ -381,4 +381,16 @@ function testStrict(val) {
   }
   testStrictNotEqual(10);
 
+ /* task 65 */
+ function testGreaterThan(val) {
+    if (val>100) {  // Change this line
+      return "Over 100";
+    }
+    if (val>10) {  // Change this line
+      return "Over 10";
+    }
+    return "10 or Under";
+  }
+  testGreaterThan(10);
+
   

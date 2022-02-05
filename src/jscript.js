9badd3716e5201a0f843bed386c90c391b69de04
shubham-saini0 <shubhamsaini@cedcoss.com>
2022-02-05 11:41:53 +0530

    Declare a Read-Only Variable with the const Keyword

--- src/jscript.js
+++ src/jscript.js
@@ -47,3 +47,10 @@ titleCaseOver = 9000;
 /* task9 */
 let catName = "Oliver";
 let catSound = "Meow!";
+
+/* task10 */
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+

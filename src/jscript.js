e2d7474c0adacadd149a475acbf38805a75c3e44
shubham-saini0 <shubhamsaini@cedcoss.com>
2022-02-05 17:33:39 +0530

    Manipulating Complex Objects

--- src/jscript.js
+++ src/jscript.js
@@ -780,4 +780,28 @@ const myDog = {
     else {
     return "Not Found";}
   }
-  
+
+  /* task 92 */
+  const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+  myMusic.push(  {
+      "artist": "Bob",
+      "title": "Hello",
+      "release_year": 1950,
+      "formats": [
+        "CS",
+        "8T"
+      ]
+    });
+    

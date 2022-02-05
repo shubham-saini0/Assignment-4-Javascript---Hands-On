ef14cad94f1a677285ded33b0fa11957ced5bc94
shubham-saini0 <shubhamsaini@cedcoss.com>
2022-02-05 15:15:49 +0530

    Comparisons with the Logical Or Operator

--- src/jscript.js
+++ src/jscript.js
@@ -440,4 +440,18 @@ function testStrict(val) {
   }
   testLogicalAnd(10);
 
+/* task 70 */
+function testLogicalOr(val) {
+    // Only change code below this line
+    if (val<10 || val>20) {
+      return "Outside";
+    }
+    else{
+      return "Inside";
+    }
+    // Only change code above this line
+    return "Inside";
+  }
+  testLogicalOr(15);
+
   

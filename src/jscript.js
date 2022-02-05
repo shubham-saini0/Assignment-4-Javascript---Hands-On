eb4661baab777b1d872037299ad5efeeb8aa5078
shubham-saini0 <shubhamsaini@cedcoss.com>
2022-02-05 11:31:26 +0530

    Understanding Uninitialized Variables

--- src/jscript.js
+++ src/jscript.js
@@ -22,3 +22,14 @@ var a=9;
 var myFirstName="Shubham";
 var myLastName="Saini";
 
+/* task7 */
+// Only change code below this line
+var a=5;
+var b=10;
+var c="I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+

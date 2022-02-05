9cc7e6f0bb2deb9ce304b9956d50e8e30272d2da
shubham-saini0 <shubhamsaini@cedcoss.com>
2022-02-05 15:24:50 +0530

    Introducing Else If Statements

--- src/jscript.js
+++ src/jscript.js
@@ -469,4 +469,20 @@ function testLogicalOr(val) {
   }
   testElse(4);
 
+/* task 72 */
+function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    }
+    else if (val < 5) {
+      return "Smaller than 5";
+    }
+   else {return "Between 5 and 10";
+  }
+  }
+  testElseIf(0);
+  testElseIf(5);
+  testElseIf(7);
+  testElseIf(10);
+  testElseIf(12);
   
